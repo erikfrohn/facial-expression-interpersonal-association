ef4b1477e0c09232dc71b31a8fc3956cf0979513
Erik Frohn <eriktfrohn@gmail.com>
2025-03-19 15:25:01 +0100

    corrca and factor fix fully working

--- util/feature_selection.py
+++ util/feature_selection.py
@@ -5,12 +5,12 @@ import util.CorrCA as CorrCA
 
 def au_to_factors(df):
     # Combine action units into factors
-    f1 = df[' AU06_r'].values + df[' AU07_r'].values + df[' AU12_r'].values
-    f2 = df[' AU01_r'].values + df[' AU02_r'].values 
-    f3 = df[' AU20_r'].values + df[' AU25_r'].values + df[' AU26_r'].values
-    f4 = df[' AU14_r'].values + df[' AU17_r'].values + df[' AU23_r'].values
-    f5 = df[' AU04_r'].values + df[' AU07_r'].values + df[' AU09_r'].values
-    f6 = df[' AU10_r'].values + df[' AU15_r'].values + df[' AU17_r'].values
+    f1 = np.round(df[' AU06_r'].values + df[' AU07_r'].values + df[' AU12_r'].values ,2)
+    f2 = np.round(df[' AU01_r'].values + df[' AU02_r'].values, 2)
+    f3 = np.round(df[' AU20_r'].values + df[' AU25_r'].values + df[' AU26_r'].values, 2)
+    f4 = np.round(df[' AU14_r'].values + df[' AU17_r'].values + df[' AU23_r'].values, 2)
+    f5 = np.round(df[' AU04_r'].values + df[' AU07_r'].values + df[' AU09_r'].values, 2)
+    f6 = np.round(df[' AU10_r'].values + df[' AU15_r'].values + df[' AU17_r'].values, 2)
 
     factors_df = pd.DataFrame({
         'frame': df['frame'],
@@ -24,16 +24,20 @@ def au_to_factors(df):
     
     return factors_df
 
-def corrCA(file_location, df_nav, df_pil):
+def corrCA_weights(file_location, df_nav, df_pil):
+    # extract needed columns
     df_nav = df_nav[[' AU01_r', ' AU02_r', ' AU04_r', ' AU05_r', ' AU06_r', ' AU07_r', ' AU09_r',
                           ' AU10_r', ' AU12_r', ' AU14_r', ' AU15_r', ' AU17_r', ' AU20_r', ' AU23_r',
                           ' AU25_r', ' AU26_r', ' AU45_r']]
     df_pil = df_pil[[' AU01_r', ' AU02_r', ' AU04_r', ' AU05_r', ' AU06_r', ' AU07_r', ' AU09_r',
                           ' AU10_r', ' AU12_r', ' AU14_r', ' AU15_r', ' AU17_r', ' AU20_r', ' AU23_r',
                           ' AU25_r', ' AU26_r', ' AU45_r']]
+    
+    # reshape for the corrca function and round
     df_nav = np.round(df_nav.T.values,2)
     df_pil = np.round(df_pil.T.values,2)
 
+    # TODO: this should not be needed and should be fixed in pre
     if df_nav.size != df_pil.size:
         if df_nav.size < df_pil.size:
             df_pil = df_pil[:,:df_nav.shape[1]]
@@ -47,28 +51,23 @@ def corrCA(file_location, df_nav, df_pil):
     # Apply CorrCA to the pair
     W, ISC, _ = CorrCA.fit(pair_data)
 
-    # W contains the weights for the 17 AUs
-    #print("Weights (W):", W)
-    #print("Inter-Subject Correlation (ISC):", ISC)
-    
+    ## start output creation
+    # first the weights and the inter-subject
     corrCA_df = pd.DataFrame(W[:,0], columns=['w']) # TODO: check whether you want multiple components!
     corrCA_df['isc'] = ISC
-    corrCA_df.to_csv(os.path.join(file_location, "corrca.csv"), index=False)
+    corrCA_df.to_csv(os.path.join(file_location, "corrca_weights.csv"), index=False)
 
+def apply_corrCA_weights(au_data, w):
+    frames = au_data['frame']
+    au_data = au_data[[' AU01_r', ' AU02_r', ' AU04_r', ' AU05_r', ' AU06_r', ' AU07_r', ' AU09_r',
+                          ' AU10_r', ' AU12_r', ' AU14_r', ' AU15_r', ' AU17_r', ' AU20_r', ' AU23_r',
+                          ' AU25_r', ' AU26_r', ' AU45_r']]
     # Transform the data using weights
-    pair_data = np.transpose(pair_data, (0, 2, 1))
-    Y = np.dot(pair_data, W)  # Shape: (2, 17, T)
+    Y = np.dot(au_data, w['w'])  # Shape: (W, T) now using W=1 so not interesting 
 
-    # Extract the first component for each participant
-    participant1_component = Y[0, :, 0]  # Shape: (T,) # TODO: check whether you want multiple components!
-    participant2_component = Y[1, :, 0]  # Shape: (T,)
-
-    # Create a DataFrame for the first component
-    first_component_df = pd.DataFrame({
-        'time': range(len(participant1_component)),  # Time points
-        'navigator': participant1_component,      # First component for Participant 1
-        'pilot': participant2_component       # First component for Participant 2
-    })
-
-    # Save the DataFrame to a CSV file
-    first_component_df.to_csv(os.path.join(file_location, "corrca_component1.csv"), index=False)
+    corrca_df = pd.DataFrame({
+        'frame': frames,
+        'component1': Y[0]
+    }
+    )
+    return corrca_df
