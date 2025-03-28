42c2991100ca5c1903971728c53df60f5aa72297
Erik Frohn <eriktfrohn@gmail.com>
2025-03-28 13:15:13 +0100

    changed some input

--- util/feature_extraction.py
+++ util/feature_extraction.py
@@ -8,10 +8,9 @@ def extract_features(input, id, output):
     openface_path = r"C:\Users\Erik\Documents\facial-expression-synchrony\OpenFace_2.2.0_win_x64\OpenFace_2.2.0_win_x64\OpenFace_2.2.0_win_x64\FeatureExtraction.exe"
    
     role = 'navigator' if int(id) % 2 != 0 else 'pilot'  # navigator = odd, pilot = even
-    
-    df_phases = pd.read_csv(os.path.join(input, "Video", f"pp{id}_{role}_video_frames.csv"))
+    file = os.path.join(input, "Video", f"pp{id}_{role}_video_frames.csv")
+    df_phases = pd.read_csv(file)
 
-    processes = []
     # iterate over all phases
     for _, row in df_phases.iterrows():
         input_file = f"pp{id}_{role}_reconstructed_video_and_audio_{row['name']}.mp4"
