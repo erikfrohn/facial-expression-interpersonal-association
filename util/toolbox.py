9a104836274c4eb36858513557a204a940e0e164
Erik Frohn <eriktfrohn@gmail.com>
2025-06-02 13:33:54 +0200

    added factor labels and code for factor gridsearch

--- util/toolbox.py
+++ util/toolbox.py
@@ -13,6 +13,14 @@ FEATURE_FOLDER = "features"
 PHASES = [f'{name}_{i}' for name, num in  [("instructional_video", 1), ("discussion_phase", 2), ('reschu_run',8)] for i in range(num)]#, ("reschu_run", 8)] for i in range(num)]
 FACTORS = ['f1', 'f2', 'f3', 'f4', 'f5', 'f6']
 
+FACTOR_LABELS = {
+    'f1': "Enjoyment Smile",
+    'f2': "Eyebrows Up",
+    'f3': "Mouth Open",
+    'f4': "Mouth Tightening",
+    'f5': "Eye Tightening",
+    'f6': "Mouth Frown"
+}
 
 def crqa_radius_gridsearch(name = "gridsearch", radii = [0.1, 0.2, 0.3, 0.4, 0.5], AVAILABLE_PAIRS = ['05_06', '07_08', '09_10', '103_104', '27_28', '83_84', '85_86', '87_88', '91_92', '93_94', '95_96', '97_98'] ):
     # Initialize DataFrame to store all results
@@ -167,14 +175,14 @@ def plot_crqa_radius_gridsearch(results_df, name='gridsearch'):
         ax.text(0.5, 0.021, 'RR=0.02', color='gray', ha='center', va='bottom', fontsize=9)
         ax.text(0.5, 0.051, 'RR=0.05', color='gray', ha='center', va='bottom', fontsize=9)
         
-        ax.set_title(title, pad=12)
+        ax.set_title(title)
         ax.set_xlabel('Radius', labelpad=8)
         ax.set_ylabel('RR ± 95% CI', labelpad=8)
         ax.set_ylim(-0.05, 0.4)
         ax.grid(True, alpha=0.3)
         
         # Only show legend on first plot
-        if ax == axes[0,0]:
+        if ax == axes[0,2]:
             ax.legend()
         else:
             ax.get_legend().remove()
@@ -186,9 +194,90 @@ def plot_crqa_radius_gridsearch(results_df, name='gridsearch'):
         ax = axes[ax_row, ax_col]
         
         factor_data = results_df[results_df['factor'] == factor]
-        plot_component(ax, factor_data, f'Facial Factor {factor.upper()}')
+        plot_component(ax, factor_data, f'{factor}: {FACTOR_LABELS[factor]}')
 
 
-
+    fig.suptitle("Factor radius gridsearch (goal RR = 2-5%)", fontsize=16, y=0.95)
     plt.savefig(f'{name}.png', dpi=300, bbox_inches='tight')
     plt.show()
+
+
+def factor_gridsearch(AVAILABLE_PAIRS, name = "gridsearch"):
+    # Initialize DataFrame to store all results
+    results_df = pd.DataFrame(columns=[
+        'pair', 'phase', 'factor', 
+        'condition', 'value'
+    ])
+
+    def process_analysis(p1_df, p2_df, component, pair, phase, condition):
+        value = np.mean(p1_df) + np.mean(p2_df)
+
+        
+        new_row = {
+            'pair': pair,
+            'phase': phase,
+            'factor': component,
+            'condition': condition,
+            'value': value
+        }
+        
+        return new_row
+
+    # Process real pairs - Factors
+    print("\nREAL PAIRS - FACTORS\n")
+    for f in FACTORS:
+        print(f"Processing factor {f}")
+        for pair in AVAILABLE_PAIRS:
+            p1, p2 = pair.split("_")
+            for phase in PHASES:
+                p1_loc = os.path.join(LOCATION, pair, FEATURE_FOLDER, f'pp{p1}_{phase}_factors.csv')
+                p2_loc = os.path.join(LOCATION, pair, FEATURE_FOLDER, f'pp{p2}_{phase}_factors.csv')
+                
+                if os.path.exists(p1_loc) and os.path.exists(p2_loc):
+                    p1_df = pd.read_csv(p1_loc)
+                    p2_df = pd.read_csv(p2_loc)
+                    p1_values = p1_df[f].values
+                    p2_values = p2_df[f].values
+                    # With non-event matches
+                    new_row = process_analysis(
+                        p1_values, p2_values, f, pair, phase, 'real'
+                    )
+                    results_df = pd.concat([results_df, pd.DataFrame([new_row])], ignore_index=True)
+    
+
+    # Process surrogate pairs - Factors
+    print("\nSURROGATE PAIRS - FACTORS\n")
+    index_real = np.arange(len(AVAILABLE_PAIRS))
+    index_fake = np.append(np.arange(1,len(AVAILABLE_PAIRS)),0)
+
+    for f in FACTORS:
+        print(f"Processing factor {f} for surrogate pairs")
+        for i in range(len(AVAILABLE_PAIRS)):
+            pair1 = AVAILABLE_PAIRS[index_real[i]]
+            pair2 = AVAILABLE_PAIRS[index_fake[i]]
+            p1, _ = pair1.split("_")
+            _, p2 = pair2.split("_")
+            
+            for phase in PHASES:
+                p1_loc = os.path.join(LOCATION, pair1, FEATURE_FOLDER, f'pp{p1}_{phase}_factors.csv')
+                p2_loc = os.path.join(LOCATION, pair2, FEATURE_FOLDER, f'pp{p2}_{phase}_factors.csv')
+                
+                if os.path.exists(p1_loc) and os.path.exists(p2_loc):
+                    p1_df = pd.read_csv(p1_loc)
+                    p2_df = pd.read_csv(p2_loc)
+                    p1_values = p1_df[f].values
+                    p2_values = p2_df[f].values
+                    # With non-event matches
+                    new_row = process_analysis(
+                        p1_values, p2_values, f, pair, phase, 'fake'
+                    )
+                    results_df = pd.concat([results_df, pd.DataFrame([new_row])], ignore_index=True)
+                    
+ 
+    # Save results to CSV
+    results_df.to_csv(f'{name}.csv', index=False)
+    print(f"Processing complete. Results saved to {name}.csv")
+
+    # Display sample of the results
+    print("\nSample of the results DataFrame:")
+    print(results_df.head())
